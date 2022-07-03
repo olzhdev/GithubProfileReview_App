c7701b344299d87328daf7a097509c8c4acd7607
Olzh <@>
2022-07-03 13:22:36 +0600

    Initial Commit

---
+++ GHFollowers Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers Application
+//
+//  Created by MAC on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
