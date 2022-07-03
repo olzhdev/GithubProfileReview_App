be69a2946c79a06c929c78a99c2e26bb14c8867c
Olzh <@>
2022-07-03 15:44:42 +0600

    Building SearchScreen (custom elements, layout)

--- GHFollowers Application/SearchVC.swift
+++ GHFollowers Application/SearchVC.swift
@@ -8,11 +8,56 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GHTextField()
+    let callToActionuttom = GHButton(backgroundColor: .systemGreen, title: "Get Followers!")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemBackground
+        configure()
+        constraint()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    
+    
+    
+    
+    private func configure() {
+        logoImageView.image = UIImage(named: "gh-logo")
+    
+    }
+    
+    private func constraint() {
+        view.addSubview(logoImageView)
+        view.addSubview(usernameTextField)
+        view.addSubview(callToActionuttom)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 44),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            callToActionuttom.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionuttom.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionuttom.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionuttom.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
 
 }
